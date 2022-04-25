436bde72236e705634c6935fe0e060f274de6a02
tkyg <80114433+tkyg@users.noreply.github.com>
2022-04-25 14:29:52 -0400

    commit tree e5ea79ef5b821b290d833d3bf8a13c65d86048ac

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block){
+    const hQLocation = 42
+    const blocks = (block >= hQLocation) ? (block - hQLocation) : (hQLocation - block)
+    return blocks
+}
+
+
+function distanceFromHqInFeet(block){
+    const hQLocation = 42
+    const distanceInFeet = (block >= hQLocation) ? ((block - hQLocation)* 264) : ((hQLocation - block) * 264)
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(start, destination){
+    const distanceTravelled = (start >= destination) ? ((start- destination) * 264) : ((destination - start) * 264)
+    return distanceTravelled
+}
+
+function calculatesFarePrice(start, destination){
+    if (((start - destination) * -264)>=1 && ((start - destination) * -264) <= 400) {
+        return (start - destination) * -0
+    } else if (((start - destination) * 264) >= 400 && ((start - destination) * 264) <= 2000){
+        return (((start - destination) * 264) - 400) * .02;
+    } else if (((start - destination) * -264) >= 2000 && ((start - destination) * 264) <= 2500){
+        return (start - destination) * -3.125;
+    } else {
+        return 'cannot travel that far'
+    }
+}
